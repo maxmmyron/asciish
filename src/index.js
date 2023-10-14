e3f026b8afee7332bb9704c55bb8f730f90bcd3f
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-10-14 09:01:38 -0400

    add error if no name provided

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { emotes } from './emoticons.js';
 const emoticons = {
   /** @param {string} name */
   fromName: (name) => {
-    if(!name) return null;
+    if(!name) throw new Error("Error in fromName: no name argument provided");
 
     return emotes.find((emote) => emote.name === name).emote || null;
   },
