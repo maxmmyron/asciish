6b156093b00c028b4cc44b4e1ddfd65dae406967
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-10-14 10:14:34 -0400

    redo loader

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import { emotes } from './emoticons.js';
+import { emotes, ascii } from './emoticons.js';
 
-const emoticons = {
-  /** @param {string} name */
-  fromName: (name) => {
-    if(!name) throw new Error("Error in fromName: no name argument provided");
-
-    return emotes.find((emote) => emote.name === name).emote || null;
-  },
-};
-
-export { emoticons };
+/**
+ * @param {string|Buffer} source content of the resource file
+ * @param {object} [map] SourceMap data consumable by https://github.com/mozilla/source-map
+ */
+module.exports = function (source, map) {
+  for(const [key, value] of emotes) {
+    source = source.replaceAll(key, value);
+  }
+  // source = source.replaceAll(/ *console.log\(.*\);?\r?\n?/g, '');
+  return source;
+}
--- src/loaders/emoticonLoader.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string|Buffer} source content of the resource file
- * @param {object} [map] SourceMap data consumable by https://github.com/mozilla/source-map
- */
-module.exports = function (source, map) {
-  source = source.replaceAll(/ *console.log\(.*\);?\r?\n?/g, '');
-  return source;
-}
-
