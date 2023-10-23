c49aa1a3e59d6d456b951945800e928f8a0870e0
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-10-23 16:11:02 -0400

    update replacement namings

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import { runtime } from './runtime.js';
-import { emotes, ascii } from './emoticons.js';
+import { emotes, unicode } from './emoticons.js';
 
 /**
  * @param {string|Buffer} source content of the resource file
  * @param {object} [map] SourceMap data consumable by https://github.com/mozilla/source-map
  */
 export default function loader(source, map) {
-  for(const [key, value] of emotes) {
-    source = source.replaceAll(key, value);
+  for(const [regexp, metadata] of emotes) {
+    source = source.replaceAll(regexp, metadata[0]);
   }
 
-  for(const [key, value] of ascii) {
-    source = source.replaceAll(key, value);
+  for(const [regexp, metadata] of unicode) {
+    source = source.replaceAll(regexp, metadata[0]);
   }
 
   return source;
