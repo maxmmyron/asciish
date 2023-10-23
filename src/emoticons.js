321d69a23fab2af734104cf535f9e0274fe15312
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-10-23 16:09:29 -0400

    update emote/unicode mappings

--- src/emoticons.js
+++ src/emoticons.js
@@ -1,20 +1,66 @@
 /**
- * @type {Map<Regexp, string>} emotes
+ * Types of emotes
+ * @readonly
+ * @enum {string}
+ */
+const EmoteType = {
+  ANGRY: "angry",
+  HAPPY: "happy",
+  LOVE: "love",
+  MISC: "misc",
+  SAD: "sad",
+  SIMPLE: "simple",
+};
+
+/**
+ * Types of unicode
+ * @readonly
+ * @enum {string}
+ */
+const UnicodeType = {
+  ARROW: "arrow",
+  MATH: "math",
+  MISC: "misc",
+  MUSIC: "music",
+}
+
+const emoteProperties = [
+  [/&smile;/g, ":)", EmoteType.SIMPLE, EmoteType.HAPPY],
+  [/&frown;/g, ":(", EmoteType.SIMPLE, EmoteType.SAD],
+  [/&angry;/g, ">:(", EmoteType.SIMPLE, EmoteType.ANGRY],
+  [/&laugh;/g, ":D", EmoteType.SIMPLE, EmoteType.HAPPY],
+  [/&wink;/g, ";)", EmoteType.SIMPLE],
+  [/&tongue;/g, ":P", EmoteType.SIMPLE],
+  [/&happy;/g, "ヽ(´▽`)/", EmoteType.HAPPY],
+  [/&cute;/g, "•‿•", EmoteType.HAPPY],
+  [/&sad;/g, "(╥_╥)", EmoteType.SAD],
+  [/&kiss;/g, "( ˘ ³˘)♥", EmoteType.LOVE],
+  [/&flip;/g, "(╯°□°)╯︵ ┻━┻", EmoteType.ANGRY],
+  [/&dog;/g, "•ᴥ•", EmoteType.MISC],
+  [/&shrug;/g, "¯\\_(ツ)_/¯", EmoteType.MISC]
+]
+
+/**
+ * @type {Map<RegExp, Array<*>>} emotes
  */
 const emotes = new Map();
 
-emotes.set(/&smile;/g, ":)");
-emotes.set(/&frown;/g, ":(");
-emotes.set(/&laugh;/g, ":D");
-emotes.set(/&wink;/g, ";)");
+for(const emote of emoteProperties) emotes.set(emote[0], emote.slice(1));
 
-const ascii = new Map();
+const unicodeProperties = [
+  [/&gt;/g, ">", UnicodeType.ARROW, UnicodeType.MATH],
+  [/&lt;/g, "<", UnicodeType.ARROW, UnicodeType.MATH],
+  [/&larr;/g, "<-", UnicodeType.ARROW],
+  [/&rarr;/g, "->", UnicodeType.ARROW],
+  [/&trebleclef;/g, "𝄞", UnicodeType.MISC],
+];
 
-ascii.set(/&gt;/g, ">");
-ascii.set(/&lt;/g, "<");
-ascii.set(/&larr;/g, "<-");
-ascii.set(/&rarr;/g, "->");
-ascii.entries(/&clef;/g, "𝄞");
+/**
+ * @type {Map<RegExp, Array<*>>} emotes
+ */
+const unicode = new Map();
 
-export { emotes, ascii };
+for(const emote of unicodeProperties) unicode.set(emote[0], emote.slice(1));
+
+export { emotes, unicode };
 
