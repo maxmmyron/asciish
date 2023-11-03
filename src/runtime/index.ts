0368c0badad10724fc9d4b93a6b7d09cf403f8d1
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-11-03 19:02:20 -0400

    generalize unicode font formatting

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,22 +1,41 @@
 import { emotes as emoteMap } from "../emotes"
 import { unicode as unicodeMap } from "../unicode"
 
+// type FontMapping = {capital: [number, number], lowercase: [number, number], digits: [number, number]};
+
+const reformatToUnicodeFont = (str: string, capIndex: number, lowerIndex: number, digitIndex: number, miscMap: Map<string, number> = new Map()) => {
+  return str.split('').map((char) => {
+    const charCode = char.charCodeAt(0);
+
+    // for some "font" mappings, some characters are explicitly defined in an
+    // earlier unicode plane, so we need to check for these edge cases before
+    // anything else
+    if(miscMap.size && miscMap.has(char))
+      return String.fromCodePoint(miscMap.get(char)!);
+
+    if(charCode >= 0x41 && charCode <= 0x5A)
+      return String.fromCodePoint(capIndex + charCode - 0x41);
+
+    if(charCode >= 0x61 && charCode <= 0x7A)
+      return String.fromCodePoint(lowerIndex + charCode - 0x61);
+
+    if(charCode >= 0x30 && charCode <= 0x39)
+      return String.fromCodePoint(digitIndex + charCode - 0x30);
+
+    return char;
+  })
+}
+
 const fanciful = {
   /**
    * Rerenders the alphabetical characters in a string using monospace unicode glyphs
    * @param {string} str the string to rerender using monospace unicode glyphs
    * @returns {string} the string with monospace unicode glyphs (U+1D670-U+1D6A3)
    */
-  toMono: (str: string) => str.split('').map((char) => {
-    console.log(char.charCodeAt(0), char.charCodeAt(0).toString(16));
-    // if capital letter
-    if (char.charCodeAt(0) >= 0x41 && char.charCodeAt(0) <= 0x5A)
-      return String.fromCodePoint(0x1D670 + char.charCodeAt(0) - 0x41);
-    if (char.charCodeAt(0) < 0x61 || char.charCodeAt(0) > 0x7A) return char;
-
-    //0x1D6A3 - 0x00061 = 0x1D642
-    return String.fromCodePoint(0x1D68A + char.charCodeAt(0) - 0x61);
-  }).join(''),
+  toMono: (str: string) => reformatToUnicodeFont(str, 0x1D670, 0x1D68A, 0x1D7F6),
+  toSerifBold: (str: string) => reformatToUnicodeFont(str, 0x1D400, 0x1D41A, 0x1D7CE),
+  toScriptBold: (str: string) => reformatToUnicodeFont(str, 0x1D4D0, 0x1D4EA, 0x1D7CE),
+  toGothicBold: (str: string) => reformatToUnicodeFont(str, 0x1D5A0, 0x1D5BA, 0x1D7CE),
 }
 
 const emotes = {
