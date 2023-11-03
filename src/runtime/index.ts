5e49edd5463a56a757c9a1824520d20f326dd170
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-11-03 16:02:57 -0400

    Fix Unicode character conversion

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -11,11 +11,11 @@ const fanciful = {
     // if capital letter
     if (char.charCodeAt(0) < 0x41 || char.charCodeAt(0) > 0x5A)
       //0x1D6A3 - 0x00041 = 0x1D662
-      return String.fromCharCode(char.charCodeAt(0) + 0x1D662);
+      return String.fromCodePoint(char.charCodeAt(0) + 0x1D662);
     if (char.charCodeAt(0) < 0x61 || char.charCodeAt(0) > 0x7A) return char;
 
     //0x1D6A3 - 0x00061 = 0x1D642
-    return String.fromCharCode(char.charCodeAt(0) + 0x1D642);
+    return String.fromCodePoint(char.charCodeAt(0) + 0x1D642);
   }).join(''),
 }
 
