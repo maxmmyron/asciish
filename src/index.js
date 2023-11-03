0489780af024eef97bc8be0d6a866b0ecc9c730b
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-11-03 14:39:15 -0400

    update exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,78 @@
-import emotes, { EmoteType } from './emoticon';
-import unicode, { UnicodeType } from './unicode';
+import * as runtime from "./runtime/index.js";
+
+/**
+ * Types of emotes
+ * @readonly
+ * @enum {string}
+ */
+export const EmoteType = {
+  ANGRY: "angry",
+  HAPPY: "happy",
+  LOVE: "love",
+  MISC: "misc",
+  SAD: "sad",
+  SIMPLE: "simple",
+};
+
+const emoteProperties = [
+  [/&smile;/g, ":)", EmoteType.SIMPLE, EmoteType.HAPPY],
+  [/&frown;/g, ":(", EmoteType.SIMPLE, EmoteType.SAD],
+  [/&angry;/g, ">:(", EmoteType.SIMPLE, EmoteType.ANGRY],
+  [/&laugh;/g, ":D", EmoteType.SIMPLE, EmoteType.HAPPY],
+  [/&wink;/g, ";)", EmoteType.SIMPLE],
+  [/&tongue;/g, ":P", EmoteType.SIMPLE],
+  [/&happy;/g, "ヽ(´▽`)/", EmoteType.HAPPY],
+  [/&cute;/g, "•‿•", EmoteType.HAPPY],
+  [/&sad;/g, "(╥_╥)", EmoteType.SAD],
+  [/&kiss;/g, "( ˘ ³˘)♥", EmoteType.LOVE],
+  [/&flip;/g, "(╯°□°)╯︵ ┻━┻", EmoteType.ANGRY],
+  [/&dog;/g, "•ᴥ•", EmoteType.MISC],
+  [/&shrug;/g, "¯\\_(ツ)_/¯", EmoteType.MISC]
+]
+
+/**
+ * @type {Map<RegExp, Array<*>>} emotes
+ */
+const emotes = new Map();
+
+for(const emote of emoteProperties) emotes.set(emote[0], emote.slice(1));
+
+/**
+ * Types of unicode
+ * @readonly
+ * @enum {string}
+ */
+export const UnicodeType = {
+  ARROW: "arrow",
+  MATH: "math",
+  MISC: "misc",
+  MUSIC: "music",
+}
+
+const unicodeProperties = [
+  [/&gt;/g, ">", UnicodeType.ARROW, UnicodeType.MATH],
+  [/&lt;/g, "<", UnicodeType.ARROW, UnicodeType.MATH],
+  [/&larr;/g, "<-", UnicodeType.ARROW],
+  [/&uarr;/g, "↑", UnicodeType.ARROW],
+  [/&rarr;/g, "->", UnicodeType.ARROW]
+  [/&darr;/g, "↓", UnicodeType.ARROW],
+  [/&trebleclef;/g, "𝄞", UnicodeType.MISC],
+  [/&product;/g, "∏", UnicodeType.MATH],
+  [/&sum;/g, "∑", UnicodeType.MATH],
+  [/&infinity;/g, "∞", UnicodeType.MATH],
+  [/&pi;/g, "π", UnicodeType.MATH],
+
+  [/&box;/g, "□", UnicodeType.MISC],
+  [/&boxcross;/g, "✖", UnicodeType.MISC],
+  [/&boxcheck;/g, "✓", UnicodeType.MISC],
+];
+
+/**
+ * @type {Map<RegExp, Array<*>>} emotes
+ */
+const unicode = new Map();
+
+for(const emote of unicodeProperties) unicode.set(emote[0], emote.slice(1));
 
 /**
  * @param {string|Buffer} source content of the resource file
@@ -17,4 +90,4 @@ export default function loader(source, map) {
   return source;
 }
 
-export { emotes, unicode, EmoteType, UnicodeType };
+export { emotes, unicode, runtime };
--- src/runtime/index.js
+++ src/runtime/index.js
@@ -1,4 +1,44 @@
-import fanciful from "./fanciful";
-import runtime from './runtime';
+/**
+ * Abc
+ */
+const fanciful = {
+  /**
+   * Rerenders the alphabetical characters in a string using monospace unicode glyphs
+   * @param {string} str the string to rerender using monospace unicode glyphs
+   * @returns {string} the string with monospace unicode glyphs (U+1D670-U+1D6A3)
+   */
+  toMono: (str) => str.split('').map((char) => {
+    // if capital letter
+    if (char.charCodeAt(0) < 0x41 || char.charCodeAt(0) > 0x5A)
+      //0x1D6A3 - 0x00041 = 0x1D662
+      return String.fromCharCode(char.charCodeAt(0) + 0x1D662);
+    if (char.charCodeAt(0) < 0x61 || char.charCodeAt(0) > 0x7A) return char;
 
-export { fanciful, runtime };
+    //0x1D6A3 - 0x00061 = 0x1D642
+    return String.fromCharCode(char.charCodeAt(0) + 0x1D642);
+  }).join(''),
+}
+
+const emotes = {
+  /**
+   * Returns a random defined emote replacement
+   *
+   * @returns {string} random emote character
+   */
+  random: () => {
+    return [...emotes.values()][Math.floor(Math.random() * emotes.size)];
+  },
+}
+
+const ascii = {
+  /**
+   * Returns a random defined ascii replacement
+   *
+   * @returns {string} random ascii character
+   */
+  random: () => {
+    return [...ascii.values()][Math.floor(Math.random() * ascii.size)];
+  }
+}
+
+export { fanciful, emotes, ascii };
