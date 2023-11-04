872f8ae546d951783df7a743cb612fea181daa79
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-11-03 20:15:46 -0400

    fix plugin export

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,28 @@ import * as runtime from "./runtime";
 import { emotes } from "./emotes"
 import { unicode } from "./unicode"
 
-/**
- * @param {string|Buffer} source content of the resource file
- * @param {object} [map] SourceMap data consumable by https://github.com/mozilla/source-map
- */
-export default function loader(source: string, map: unknown) {
-  for(const [regexp, metadata] of emotes) {
-    source = source.replaceAll(regexp, metadata[0]);
-  }
+function rollupPlugin() {
+  return {
+    name: "asciish-transform-shortcodes",
 
-  for(const [regexp, metadata] of unicode) {
-    source = source.replaceAll(regexp, metadata[0]);
-  }
+    transform: (src: string, id: string) =>  {
+      if(id.includes("node_modules/") || id.includes("dist/")) return;
 
-  return source;
+      let source = src.toString();
+
+      for(const [regexp, metadata] of emotes) {
+        source = source.replaceAll(regexp, metadata[0]);
+      }
+
+      for(const [regexp, metadata] of unicode) {
+        source = source.replaceAll(regexp, metadata[0]);
+      }
+
+      return {
+        code: source,
+      };
+    }
+  }
 }
 
-export { emotes, unicode, runtime };
+export { rollupPlugin, emotes, unicode, runtime };
