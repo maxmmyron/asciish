860115d96692fe47c1ba7c50a776917f4dd194f3
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-11-11 10:53:18 -0500

    add prose

--- src/prose.ts
+++ src/prose.ts
@@ -2,5 +2,7 @@ const punctuation: Map<RegExp, string> = new Map();
 
 punctuation.set(/--/g, '—');
 punctuation.set(/!?/g, '‽');
+punctuation.set(/.../g, '…');
+punctuation.set(/\(c\)/gi, '©');
 
 export { punctuation };
